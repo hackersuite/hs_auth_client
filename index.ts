b0d8cb2fcd5d6e2919d015455ee4ecab96cf31d7
Heido Nomm <heidonomm@gmail.com>
2020-02-15 19:52:35 +0000

    auth tokens to method args, types, JSON parsing

--- index.ts
+++ index.ts
@@ -1,54 +1,91 @@
 import * as req from "request-promise-native";
 
+import { RequestUser, Team } from "./types";
 
-export const getCurrentUser = async (): Promise<string> => {
-  return req.get(`${process.env.AUTH_URL}/api/v1/users/me`, {
-    headers: {
-      Authorization: `${req.cookies["Authorization"]}`,
-      Referer: req.originalUrl
-    }
-  });
+
+export const getCurrentUser = async (token: string): Promise<RequestUser> => {
+  let res: string;
+  try {
+    res =  await req.get(`${process.env.AUTH_URL}/api/v1/users/me`, {
+      headers: {
+        Authorization: `${token}`,
+      }
+    })
+  } catch (err) { throw err; }
+
+  const user: any  = JSON.parse(res);
+  if (user.error && user.status >= 400 )
+    throw user.error;
+
+  return {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    password: user.password,
+    email_verified: user.email_verified,
+    auth_level: user.auth_level,
+    team: user.team
+  };
 }
 
-export const getAllUsers = async (): Promise<any> => {
-  return req.get(`${process.env.AUTH_URL}/api/v1/users`, {
-    headers: {
-      Authorization: `${req.cookies["Authorization"]}`
+
+export const getAllUsers = async (token: string): Promise<RequestUser[]> => {
+  let res;
+  try {
+    res =  await req.get(`${process.env.AUTH_URL}/api/v1/users`, {
+      headers: {
+        Authorization: `${token}`
+      }
+    });
+  } catch (err) { throw err; }
+
+  const users: any = JSON.parse(res);
+  if (users.error && users.status >= 400 ) { throw users.error; }
+  
+  return users.map( (user: RequestUser) => (
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      email_verified: user.email_verified,
+      auth_level: user.auth_level,
+      team: user.team
     }
-  });
+  ));
 }
 
-export const putCurrentUser = async (): Promise<void> => {
-  req.put(`${process.env.AUTH_URL}/api/v1/users/me`, {
+export const putCurrentUser = async (name: string, token: string): Promise<void> => {
+  const res = await req.put(`${process.env.AUTH_URL}/api/v1/users/me`, {
     headers: {
-      Authorization: `${req.cookies["Authorization"]}`
+      Authorization: `${token}`
     },
     body: {
       name: `${name}`
     }
   });
+
+  if (res.error && res.status >= 400 ) { throw res.error; }
 }
 
-export const getTeams = async (): Promise<string> => {
-  return req.get(`${process.env.AUTH_URL}/api/v1/teams/`, {
-    headers: {
-      Authorization: `${req.cookies["Authorization"]}`
-    }
-  });
-}
+export const getTeams = async (token: string): Promise<Team[]> => {
+  let res: string;
+  try {
+    res = await req.get(`${process.env.AUTH_URL}/api/v1/teams/`, {
+      headers: {
+        Authorization: `${token}`,
+      }
+    })
+  } catch (err) { throw err; }
 
-export const getTeam = async (teamCode: string ): Promise<string> => {
-  return req.get(`${process.env.AUTH_URL}/api/v1/teams/${teamCode}`, {
-    headers: {
-      Authorization: `${req.cookies["Authorization"]}`
-    }
-  });
-}
+  const teams: any = JSON.parse(res);
+  if (teams.error && teams.status >= 400 ) { throw teams.error; }
 
-export const getTeamMembers = async (teamCode: string): Promise<string> => {
-  return req.get(`${process.env.AUTH_URL}/api/v1/teams/${teamCode}/members`, {
-    headers: {
-      Authorization: `${req.cookies["Authorization"]}`
+  return teams.map( (team: Team) => (
+    {
+      _id: team._id,
+      name: team.name,
+      creator: team.creator
     }
-  });
+  ))
 }
