52f8fe8022c8d908969239e8782b15f23cf40f55
Amish Shah <amishshah.2k@gmail.com>
2020-06-09 17:11:28 +0100

    feat: add getTeams tests

---
+++ tests/fixtures/teams.js
@@ -0,0 +1,11 @@
+exports.Team1 = {
+	'_id': '5e2f142e714294ee19c1ad2a',
+	'name': 'TestTeam',
+	'creator': '5e2efe22714294ee19c1ad29'
+};
+
+exports.Team2 = {
+	'_id': '1238912494528342938493',
+	'name': 'Team 3',
+	'creator': '19230123809290'
+};
