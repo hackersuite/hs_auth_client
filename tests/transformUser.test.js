91282430f31d3afda0a2ac730d718ae0c39793e4
Amish Shah <amishshah.2k@gmail.com>
2020-06-15 19:17:23 +0100

    test: test transformUser/transformExtendedUser independently

---
+++ tests/transformUser.test.js
@@ -0,0 +1,27 @@
+const { transformUser, transformExtendedUser } = require('../dist/util/transformUser');
+const { sanitiseTeam } = require('../dist/util/sanitiseTeam');
+const { convertAuthLevel } = require('../dist/util/authLevel');
+const fixtures = require('./fixtures/users');
+
+test('Users are transformed correctly', () => {
+	for (const fixture of [fixtures.JohnDoe, fixtures.BobRoss]) {
+		const transformed = transformUser(fixture);
+		expect(transformUser(fixture)).toEqual({
+			id: fixture._id,
+			name: fixture.name,
+			email: fixture.email,
+			team: sanitiseTeam(fixture.team),
+			authLevel: convertAuthLevel(fixture.auth_level)
+		});
+	}
+});
+
+test('Exended users are transformed correctly', () => {
+	for (const fixture of [fixtures.JohnDoe, fixtures.BobRoss]) {
+		const transformed = transformUser(fixture);
+		expect(transformExtendedUser(fixture)).toEqual({
+			...transformUser(fixture),
+			emailVerified: fixture.email_verified
+		});
+	}
+});
